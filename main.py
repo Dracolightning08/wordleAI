6f7b84319ecfe418cb00d59e3269b7d36b1c1d2c
Dracolightning08 <125089303+Dracolightning08@users.noreply.github.com>
2023-09-10 14:31:45 -0400

    updated comments in regards to important project information on how it runs.

--- main.py
+++ main.py
@@ -8,6 +8,27 @@
                                           Tre Cobb                           
                                                                                                      '''
 '''--------------------------------------------------------------------------------------------------
+                                    IMPORTANT PROJECT INFORMATION:
+   --------------------------------------------------------------------------------------------------
+    The following repository wordle, is a sub-repository of wordleAI. This sub-repository has been 
+    cloned inside this repo and its credit goes to github user meicholtz at the following link:
+    https://github.com/meicholtz/wordle. 
+    
+    In order to utilize the commands to play the game one must navigate using ls and cd to the directory
+    wordle to be able to run them or else it will result in error. 
+    
+    For example: if you are on the main repository, execute ls to see the directories available, and then
+    execute cd wordle to enter the right repository to run the commands for the game.
+
+    Additonally in order to execute git, commit, and push commands, you must ensure that you are in
+    the wordleAI repository in order to push the changes. If you are in wordl directory then try to run
+    those commands it actually tries to send the changes to the meicholz world repository which access
+    will be immediately denied.
+    
+    Execute cd .. to go back to wordleAI directory if in wordle directory
+   ---------------------------------------------------------------------------------------------------'''
+
+'''--------------------------------------------------------------------------------------------------
                                     WordleAI Project Description:
    --------------------------------------------------------------------------------------------------
     
@@ -26,12 +47,12 @@ import pdb # import a python debugger
 
 def main(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'\nYes! {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('PyCharm')
+    main('\nWordleAI test is a success')
 
 '''--------------------------------------------------------------------------------------------------
                                             Program End:
